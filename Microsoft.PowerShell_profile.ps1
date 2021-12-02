dabbf80a5f25f8d25dab2c19e1e79f9075773dba
a2250180 <u09900161@gmail.com>
2021-12-02 09:22:38 +0800

    Create Microsoft.PowerShell_profile.ps1
    Powershell main file

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,5 @@
+$OutputEncoding = [console]::InputEncoding = [console]::OutputEncoding = [Text.UTF8Encoding]::UTF8
+
+Import-Module D:\PowerShell\Help.psm1 -Force
+Import-Module D:\PowerShell\QueryWebUrl.psm1 -Force
+Import-Module D:\PowerShell\QueryAllWebUrlKey.psm1 -Force

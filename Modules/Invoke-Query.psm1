5fe76b51118607817305d093d1490dade0ef879d
Skadooshhhhhh <u09900161@gmail.com>
2021-12-03 10:14:10 +0800

    feat: add Invoke query
    feat: add Invoke query

---
+++ Modules/Invoke-Query.psm1
@@ -0,0 +1,22 @@
+function Invoke-Query {
+    param(
+        [string] $dataSource = "***",
+        [string] $database = "***",
+        [string] $sqlCommand = $(Get-Content -Path ***.sql)
+      )
+
+    $connectionString = "Data Source=$dataSource; " +
+            "Integrated Security=SSPI; " +
+            "Initial Catalog=$database"
+
+    $connection = new-object system.data.SqlClient.SQLConnection($connectionString)
+    $command = new-object system.data.sqlclient.sqlcommand($sqlCommand,$connection)
+    $connection.Open()
+    
+    $adapter = New-Object System.Data.sqlclient.sqlDataAdapter $command
+    $dataset = New-Object System.Data.DataSet
+    $adapter.Fill($dataSet) | Out-Null
+    
+    $connection.Close()
+    $dataSet.Tables | Format-Table
+}
